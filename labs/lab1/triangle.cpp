209371486ce6ac0ad7430968fc1cd4d08a451f65
Aaron Jones <ajones061392@gmail.com>
2017-02-26 19:53:11 -0800

    Finished declaring the class in the triangle.h header file and now will work on defining the functions in the triangle.cpp file.

--- labs/lab1/triangle.cpp
+++ labs/lab1/triangle.cpp
@@ -1,10 +1,10 @@
-/* 
+/*
 *  Created by: Aaron Jones
 *  Date: 2/24/17
 *  DueDate: 2/27/17
-*  Description: This is file contains the triangle class for which will contain 
-* 		the following detailed description of each function. At a later 
-*		date. 
+*  Description: This is file contains the triangle class for which will contain
+* 		the following detailed description of each function. At a later
+*		date.
 */
 
 /*The following will be used as a skeleton for the following triangle class*/
@@ -12,11 +12,11 @@
 #include <iostream>
 #include "triangle.h"
 
-using namespace std; 
+using namespace std;
 
 Triangle::Triangle()
 {
-	size
+
 }
 
 
--- labs/lab1/triangle.h
+++ labs/lab1/triangle.h
@@ -3,17 +3,28 @@
 *  Date: 2/24/17
 *  DueDate: 2/27/17
 *  Description: This header file will be used to declare the
-* 		triangle class within triangle.cpp. 
+* 		triangle class within triangle.cpp.
 */
 
 class Triangle
 {
-	public: 
+	public:
 		Triangle();
 		Triangle(int size);
 		Triangle(int size, char borderCharacter, char fillCharacter);
+
+        int getSize();
+        int getPerimeter();
+        int getArea();
+
+        void setSize(int size);
+        void setPerimeter(int perimeter);
+        void setArea(double area);
+
 	private:
 		int size;
+        int perimeter;
+        double area;
 		char borderCharacter;
 		char fillCharacter;
 
