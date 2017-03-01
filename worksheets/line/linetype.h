8af248cb193a93a2d4f53e27023112965437f177
Aaron Jones <ajones061392@gmail.com>
2017-02-28 18:09:29 -0800

    Added more stuff and finally got a working linetype.h working with main to create the LineDriver object.

--- worksheets/line/linetype.h
+++ worksheets/line/linetype.h
@@ -4,6 +4,7 @@
 #include <iostream>
 
 class lineType
+
 {
 private:
     int a_x, b_y,c_c;
@@ -20,10 +21,37 @@ public:
     int getY() {return b_y;}
     int getC() {return c_c;}
 
+    void print()
+    {
+        std::cout << "Line("<< a_x << ","<< b_y << ")" << "\n";
+    }
+
     void summary()
     {
-        std::cout << "Line("<< a_x << ", "<< b_y << "," << c_c << ")" << "\n";
+        std::cout << "\n" << "\t" << "Summary" << "\n"
+                  << "-----------------------------" << "\n"
+                  << "\n" <<  "Point A_X: " << a_x<< "\n"
+                  << "point B_Y: "<< b_y << "\n"
+                  << "Slope: " << slope << "\n";
     }
+
+    /*Overloads the unary operator +, as a member function, so that it
+     * returns true if a line is horizontal otherwise false.*/
+    friend lineType operator+(const lineType &lT1, const lineType &lT2);
+    /*Overloads the assignment operator = to copy a line into another line*/
+    friend lineType operator=(const lineType &lT1, const lineType &lT2);
+    /*Overloads the unary operator -, as a member function so that it returns
+     * true if a line is horizontal otherwise false*/
+    friend lineType operator-(const lineType &lT1, const lineType &lT2);
+    /*Overloads the operator ==, as a member function so that it returns true if
+     * two lines are equal otherwise false*/
+    friend lineType operator==(const lineType &lT1, const lineType &lT2);
+    /*Overloads the operator ||, as a member function, so that is returns true
+     * if two lines are parallel otherwise false.*/
+    friend lineType operator||(const lineType &lT1, const lineType &lT2);
+    /*Overloads the operator &&, as a member function, so that it returns true
+     * if two lines are perpendicular otherwise false. */
+    friend lineType operator&&(const lineType &lT1, const lineType&lT2);
 };
 
 #endif // LINETYPE_H
--- worksheets/line/main.cpp
+++ worksheets/line/main.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
 {
     lineType linetype1(3, 4, 5);
     lineType linetype2(2,5,8);
+
+    linetype1.print();
+    linetype2.print();
     linetype1.summary();
     linetype2.summary();
 
